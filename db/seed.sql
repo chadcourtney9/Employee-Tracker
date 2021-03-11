9b5f9d99bf55192ab8b2b3faa4f3f842abb80f0c
Chad Courtney <Chadcourtney567@gmail.com>
2021-03-11 15:01:46 -0500

    added seeds

--- db/seed.sql
+++ db/seed.sql
@@ -10,7 +10,7 @@ INSERT INTO department (name)
 VALUE ("Finance")
 
 
--- roles
+-- Roles
 INSERT INTO role (title, salary, department_id)
 VALUE ("Lead Engineer", 120000, 3)
 INSERT INTO role (title, salary, department_id)
@@ -24,4 +24,26 @@ VALUE ("Salesperson", 35000, 2)
 INSERT INTO role (title, salary, department_id)
 VALUE ("Accountant", 75000, 4)
 
--- managers
+-- Managers
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Marshall", "Mathers", 1, 1)
+INSERT INTO  employee (first_name, last_name, manager_id, department_id)
+VALUE ("Itachi", "Uchiha", 2, 2)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Minato", "Uzamaki", 3, 3)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Kaneki", "Ken", 4, 4)
+
+-- Employees
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Joe", "Lyons", 1, 5)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Patrick", "DeMarco", 2, 6)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Mike", "Fearnley", 2, 7)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Toni", "Powell", 3, 8)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Tom", "Fallon", 3, 9)
+INSERT INTO employee (first_name, last_name, manager_id, department_id)
+VALUE ("Tim", "Vigneau", 4, 10)
